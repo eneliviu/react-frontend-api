d722b4a405c0a7621d2039a021e332e118002736
Liviu Ene <eneliviu@gmail.com>
2025-02-06 17:19:44 +0100

    Heroku deployment fix axios base url

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,7 +4,7 @@ import axios from "axios";
 //axios.defaults.baseURL = "https://drf-backend-api-70211104c0c7.herokuapp.com";
 
 // axios.defaults.baseURL = "http://127.0.0.1:8000";
-axios.defaults.baseURL = "https://dj-api-backend-8cf355e96add.herokuapp.com";
+axios.defaults.baseURL = "https://react-frontend-api-b166a083b609.herokuapp.com/";
 
 
 
