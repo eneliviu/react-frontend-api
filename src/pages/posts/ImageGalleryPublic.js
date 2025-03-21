08b5e14869690d7e9f73ed4627d5310c2f7e03e4
Liviu Ene <eneliviu@gmail.com>
2025-03-21 18:09:18 +0100

    Test Heroku deployment

--- src/pages/posts/ImageGalleryPublic.js
+++ src/pages/posts/ImageGalleryPublic.js
@@ -1,3 +1,110 @@
+// import React, { useEffect, useState } from "react";
+// import Form from "react-bootstrap/Form";
+// import Col from "react-bootstrap/Col";
+// import Row from "react-bootstrap/Row";
+// import Container from "react-bootstrap/Container";
+
+// import Post from "./Post";
+// import Asset from "../../components/Asset";
+
+// import appStyles from "../../App.module.css";
+// import srcbStyles from "../../styles/SearchBar.module.css";
+// import styles from "../../styles/ImageGalleryPublic.module.css";
+
+// import { useLocation } from "react-router";
+// import { axiosReq } from "../../api/axiosDefaults";
+
+// import NoResults from "../../assets/no-results.png";
+// import InfiniteScroll from "react-infinite-scroll-component";
+// import { fetchMoreData } from "../../utils/utils";
+
+// import PopularProfiles from "../profiles/PopularProfiles";
+// import log from "../../utils/logger";
+
+
+// function ImageGalleryPublic({ message, filter = "", refresh }) {
+//     const [posts, setPosts] = useState({ results: [] });
+//     const [hasLoaded, setHasLoaded] = useState(false);
+//     const { pathname } = useLocation();
+//     const [query, setQuery] = useState("");
+
+//     useEffect(() => {
+//         const fetchPosts = async () => {
+//             try {
+//                 const { data } = await axiosReq.get(
+//                     `/gallery/?search=${query}&${filter}`
+//                 );
+//                 setPosts(data);
+//                 setHasLoaded(true);
+//             } catch (err) {
+//                 log.error(err);
+//             }
+//         };
+//         setHasLoaded(false);
+//         const timer = setTimeout(() => {
+//             fetchPosts();
+//         }, 1000);
+//         return () => clearTimeout(timer);
+//     }, [query, pathname, filter, refresh]);
+
+//     return (
+//         <div className={styles.GalleryContainer}>
+//             <Row>
+//                 <Col className="py-2 p-0 p-lg-2">
+//                     <i className={`fas fa-search ${srcbStyles.SearchIcon}`} />
+//                     <Form
+//                         className={srcbStyles.SearchBar}
+//                         onSubmit={(event) => event.preventDefault()}
+//                     >
+//                         <Form.Control
+//                             value={query}
+//                             onChange={(event) => setQuery(event.target.value)}
+//                             type="text"
+//                             placeholder="Search free text and upload date (yyyy-mm-dd)"
+//                             className="mr-sm-2"
+//                         />
+//                     </Form>
+//                     {hasLoaded ? (
+//                         <>
+//                             {posts.results.length ? (
+//                                 <InfiniteScroll
+//                                     children={posts.results
+//                                         .filter((post) => post.image)
+//                                         .map((post) => (
+//                                             <Post
+//                                                 key={post.id}
+//                                                 {...post}
+//                                                 setPosts={setPosts}
+//                                             />
+//                                         ))}
+//                                     dataLength={posts.results.length}
+//                                     loader={<Asset spinner />}
+//                                     hasMore={!!posts.next}
+//                                     next={() => fetchMoreData(posts, setPosts)}
+//                                 />
+//                             ) : (
+//                                 <Container className={appStyles.Content}>
+//                                     <Asset src={NoResults} message={message} />
+//                                 </Container>
+//                             )}
+//                         </>
+//                     ) : (
+//                         <Container className={appStyles.Content}>
+//                             <Asset spinner />
+//                         </Container>
+//                     )}
+//                 </Col>
+//                 <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+//                     <PopularProfiles />
+//                 </Col>
+//             </Row>
+//         </div>
+//     );
+// }
+
+// export default ImageGalleryPublic;
+
+
 import React, { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -21,7 +128,6 @@ import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
 import log from "../../utils/logger";
 
-
 function ImageGalleryPublic({ message, filter = "", refresh }) {
     const [posts, setPosts] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
@@ -29,6 +135,7 @@ function ImageGalleryPublic({ message, filter = "", refresh }) {
     const [query, setQuery] = useState("");
 
     useEffect(() => {
+        console.log("useEffect re-run due to refresh:", refresh);
         const fetchPosts = async () => {
             try {
                 const { data } = await axiosReq.get(
@@ -36,8 +143,10 @@ function ImageGalleryPublic({ message, filter = "", refresh }) {
                 );
                 setPosts(data);
                 setHasLoaded(true);
+                log.info("Data fetched successfully:", data); // Log successful fetch
             } catch (err) {
-                log.error(err);
+                log.error("Failed to fetch posts:", err); // Log errors
+                setHasLoaded(true); // Ensure spinner is hidden even if there's an error
             }
         };
         setHasLoaded(false);
@@ -45,7 +154,7 @@ function ImageGalleryPublic({ message, filter = "", refresh }) {
             fetchPosts();
         }, 1000);
         return () => clearTimeout(timer);
-    }, [query, pathname, filter, refresh]);
+    }, [query, pathname, filter, refresh]); // Ensure refresh is in the dependency array
 
     return (
         <div className={styles.GalleryContainer}>
@@ -103,4 +212,3 @@ function ImageGalleryPublic({ message, filter = "", refresh }) {
 }
 
 export default ImageGalleryPublic;
-
