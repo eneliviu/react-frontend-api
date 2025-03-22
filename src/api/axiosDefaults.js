bd7e28ed0b3cc4c776e5aba8e59926a31bc2d8b0
Liviu Ene <eneliviu@gmail.com>
2025-03-22 14:53:22 +0100

    Test Heroku deployment

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000";
+// axios.defaults.baseURL = "http://127.0.0.1:8000";
 
-//axios.defaults.baseURL = "https://dj-api-backend-8cf355e96add.herokuapp.com";
+axios.defaults.baseURL = "https://dj-api-backend-8cf355e96add.herokuapp.com";
 
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
